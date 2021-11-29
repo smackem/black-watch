798ecd30bd9d8d953cab6ca2b0a48668c7f89eda
smackem <sm4ckem@outlook.com>
2021-11-29 20:00:57 +0100

    - recreated BlackWatch.WebApp based on net5 instead of net6

--- src/BlackWatch.WebApp/Program.cs
+++ src/BlackWatch.WebApp/Program.cs
@@ -1,19 +1,17 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BlackWatch.WebApp
 {
-    public static class Program
+    public class Program
     {
-        internal static async Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 

408ecb4764d8efa89c9a956da0568d2a3f5e5519
Philip Boger <philip.boger@sinc.de>
2021-11-29 18:15:33 +0100

    - added razor wasm project BlackWatch.WebApp

--- src/BlackWatch.Daemon/Features/CronActions/EvaluationAction.cs
+++ src/BlackWatch.Daemon/Features/CronActions/EvaluationAction.cs
@@ -21,7 +21,7 @@ namespace BlackWatch.Daemon.Features.CronActions
             TallyService tallyService,
             IDataStore dataStore,
             ILogger logger)
-            : base(cronExpr, "evaluate hourly tally sources")
+            : base(cronExpr, $"evaluate tally sources @ interval {interval}")
         {
             _interval = interval;
             _tallyService = tallyService;
@@ -33,7 +33,7 @@ namespace BlackWatch.Daemon.Features.CronActions
         {
             var tallies = _tallyService.EvaluateAsync(_interval);
             var count = 0;
-            _logger.LogDebug("evaluating tally sources with interval {Interval}", _interval);
+            _logger.LogDebug("evaluating tally sources at interval {Interval}", _interval);
 
             await foreach (var tally in tallies.Linger())
             {
@@ -41,7 +41,7 @@ namespace BlackWatch.Daemon.Features.CronActions
                 count++;
             }
 
-            _logger.LogInformation("evaluating tally sources with interval {Interval} yielded {TallyCount} tallies", _interval, count);
+            _logger.LogInformation("evaluating tally sources at interval {Interval} yielded {TallyCount} tallies", _interval, count);
             return true;
         }
     }
